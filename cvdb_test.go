13ec42d9ce49b1870181e1f8f1726a414d23ea8b
Sven Engelhardt <sven@iptools.de>
2016-07-16 17:38:46 +0200

    add iteration over all keys in a database

--- cvdb_test.go
+++ cvdb_test.go
@@ -4,62 +4,96 @@ import (
 	"bytes"
 	"fmt"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 )
 
-func TestIt001(t *testing.T) {
-	const (
-		dbname  = "/var/tmp/__cvdb_test.db"
-		numkeys = 1000000
-	)
-	opts := Options{
-		Offset: 1024,
+const (
+	dbname  = "/var/tmp/__cvdb_test.db"
+	numkeys = 1000000
+)
+
+var opts = Options{
+	Offset: 1024,
+}
+
+func TestWrite001(t *testing.T) {
+	tstart := time.Now()
+	db, err := CreateOpts(dbname, &opts)
+	//db.dumpDistrib = true
+	if err != nil {
+		t.Fatalf("error in create: %v", err)
 	}
-	{
-		tstart := time.Now()
-		db, err := CreateOpts(dbname, &opts)
-		//db.dumpDistrib = true
+	for i := 0; i < numkeys; i++ {
+		ks := "test-" + strconv.FormatInt(int64(i), 36) + "-test"
+		db.Put([]byte(ks), []byte(ks))
+	}
+	if err := db.Commit(); err != nil {
+		t.Fatalf("error in commit: %v", err)
+	}
+	db.Close()
+	dura := time.Since(tstart)
+	fmt.Printf("wrote %v keys in %v (%v/key), %v keyskips\n", numkeys, dura, dura/numkeys, db.skipped)
+}
+
+func TestRead001(t *testing.T) {
+	tstart := time.Now()
+	var tfirst time.Duration
+	db, err := OpenOpts(dbname, &opts)
+	if err != nil {
+		t.Fatalf("error in open: %v", err)
+	}
+	buffer := make([]byte, 2028)
+	for i := 0; i < numkeys; i++ {
+		ks := "test-" + strconv.FormatInt(int64(i), 36) + "-test"
+		val, err := db.GetBuffer([]byte(ks), buffer)
 		if err != nil {
-			t.Fatalf("error in create: %v", err)
+			t.Fatalf("read error %v", err)
 		}
-		for i := 0; i < numkeys; i++ {
-			ks := "test-" + strconv.FormatInt(int64(i), 36)
-			db.Put([]byte(ks), []byte(ks))
+		if val == nil {
+			t.Fatalf("could not read key %v", ks)
 		}
-		if err := db.Commit(); err != nil {
-			t.Fatalf("error in commit: %v", err)
+		if !bytes.Equal([]byte(ks), val) {
+			t.Fatalf("key %v returned invalid value", ks)
 		}
-		db.Close()
-		fmt.Printf("wrote %v keys in %v, %v keyskips\n", numkeys, time.Since(tstart), db.skipped)
+		if i == 0 {
+			tfirst = time.Since(tstart)
+		}
 	}
-	{
-		tstart := time.Now()
-		var tfirst time.Duration
-		db, err := OpenOpts(dbname, &opts)
-		if err != nil {
-			t.Fatalf("error in open: %v", err)
-		}
-		buffer := make([]byte, 2028)
-		for i := 0; i < numkeys; i++ {
-			ks := "test-" + strconv.FormatInt(int64(i), 36)
-			val, err := db.GetBuffer([]byte(ks), buffer)
-			if err != nil {
-				t.Fatalf("read error %v", err)
-			}
-			if val == nil {
-				t.Fatalf("could not read key %v", ks)
-			}
-			if !bytes.Equal([]byte(ks), val) {
-				t.Fatalf("key %v returned invalid value", ks)
-			}
-			if i == 0 {
-				tfirst = time.Since(tstart)
-			}
-		}
-		fmt.Printf("read  %v keys in %v (time to first read %v)\n", numkeys, time.Since(tstart), tfirst)
-		db.Close()
+	dura := time.Since(tstart)
+	fmt.Printf("read  %v keys in %v (%v/key) (time to first read %v)\n", numkeys, dura, dura/numkeys, tfirst)
+	db.Close()
+}
+
+func TestIterate001(t *testing.T) {
+	tstart := time.Now()
+	db, err := OpenOpts(dbname, &opts)
+	if err != nil {
+		t.Fatalf("error in open: %v", err)
 	}
+	iter := db.Iterator()
+	cnt := 0
+	for k, v := iter.First(); k != nil; k, v = iter.Next() {
+		ks := string(k)
+		if !strings.HasPrefix(ks, "test-") && !strings.HasSuffix(ks, "-test") {
+			t.Fatalf("invalid key %v (len %v) - value %v", ks, len(k), string(v))
+		}
+		vs := string(v)
+		if vs != ks {
+			t.Fatalf("invalid value for key %v", ks)
+		}
+		cnt++
+	}
+	if err := iter.Err(); err != nil {
+		t.Fatalf("iterator error %v", err)
+	}
+	if cnt != numkeys {
+		t.Errorf("iterator returned %v keys (%v) expected", cnt, numkeys)
+	}
+	dura := time.Since(tstart)
+	fmt.Printf("iter  %v keys in %v (%v/key)\n", numkeys, dura, dura/numkeys)
+	db.Close()
 }
 
 var sV = sv()
---
+++ iterator.go
@@ -0,0 +1,67 @@
+package cvdb
+
+import "encoding/binary"
+
+type Iterator struct {
+	r   *Reader
+	tbl int
+	idx uint32
+	err error
+	buf []byte
+}
+
+func (r *Reader) Iterator() *Iterator {
+	return &Iterator{r: r, buf: make([]byte, 1024)}
+}
+
+func (it *Iterator) First() ([]byte, []byte) {
+	it.tbl = 0
+	it.idx = 0
+	return it.Next()
+}
+
+func (it *Iterator) Next() ([]byte, []byte) {
+	var scratch [16]byte
+again:
+	if it.err != nil || it.tbl == int(it.r.numBuckets) {
+		return nil, nil
+	}
+	currentBucket := it.r.index[it.tbl]
+	for it.idx < currentBucket.len {
+		fpos := int64(currentBucket.pos)
+		fpos += 12 * int64(it.idx)
+		if _, err := it.r.f.ReadAt(scratch[:12], fpos); err != nil {
+			it.err = ioerror(err)
+			return nil, nil
+		}
+		it.idx++
+		pos := int64(binary.LittleEndian.Uint64(scratch[4:12]))
+		if pos == 0 {
+			continue
+		}
+		if _, err := it.r.f.ReadAt(scratch[:8], pos); err != nil {
+			it.err = ioerror(err)
+			return nil, nil
+		}
+		kl := binary.LittleEndian.Uint32(scratch[:4])
+		vl := binary.LittleEndian.Uint32(scratch[4:])
+		bl := int(kl) + int(vl)
+		if cap(it.buf) < bl {
+			it.buf = make([]byte, bl)
+		} else {
+			it.buf = it.buf[:bl]
+		}
+		if _, err := it.r.f.ReadAt(it.buf, pos+8); err != nil {
+			it.err = ioerror(err)
+			return nil, nil
+		}
+		return it.buf[:kl], it.buf[kl:]
+	}
+	it.tbl++
+	it.idx = 0
+	goto again
+}
+
+func (it *Iterator) Err() error {
+	return it.err
+}
